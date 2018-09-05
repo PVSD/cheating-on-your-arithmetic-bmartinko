9d3a329ea02897da00641dca56c0e9a8e9f23a03
bmartinko <bennett.martinko@gmail.com>
2018-09-05 11:23:32 -0400

    I probably won't be able to find this so....

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int problemA = 79 + 3 *(4 + 82 - 68) - 7 + 19;
+        String aProblem = "79 + 3 * (4 + 82 - 68) - 7 + 19";
+        int problemB = (179 + 21 +10) / 7 + 181;
+        String bProblem = "(179 + 21 +10) / 7 + 181";
+        int problemC = 10389 * 56 * 11 + 2246;
+        String cProblem = "10389 * 56 * 11 + 2246";
+
+        System.out.println(aProblem + " = " + problemA);
+        System.out.println(bProblem + " = " + problemB);
+        System.out.println(cProblem + " = " + problemC);
     }
 }
